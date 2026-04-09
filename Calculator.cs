e273e105cb3ef6a31d21fea4497a78954b700a75
Jose <j.andryan@students.amikom.ac.id>
2026-04-09 10:15:56 +0700

    menambahkan fungsi Main

--- Calculator.cs
+++ Calculator.cs
@@ -1,10 +1,17 @@
 class Calcuator
 {
 	static void Main(string[] args)
-		{
-			
-		}
+	{
+		int a = 10;
+		int b = 6;
+		
+		console.writeLine("hasil penambahan: {0} + {1} ={2}", a, b, penambahan(a,b));
+		console.writeLine("hasil penambahan: {0} - {1} ={2}", a, b, pengurangan(a,b));
+		
+		console.writeLine("\ntekan sembarang key untuk keluar");
+		console.ReadKey();
 	}
+	
 	static int penambahan(int a, int b)
 	{
 		return a + b;
